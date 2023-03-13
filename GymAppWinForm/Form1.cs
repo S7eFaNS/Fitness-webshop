3a232a1e70749c5304ca460293a82c0cd0076e0c
Stefan-Nikola <s.stanev@student.fontys.nl>
2023-03-13 10:07:39 +0100

    Pushing some small changes

---
+++ GymAppWinForm/Form1.cs
@@ -0,0 +1,20 @@
+using ClassLibrary;
+using GymProject.Classes;
+
+namespace GymAppWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Administration administration = new Administration();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
